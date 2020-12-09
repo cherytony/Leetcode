f2a33e771989c62b2166d06c9cbb430136a70525
zls-pc <zls-pc@zls-pcdeMacBook-Air.local>
2020-12-10 00:33:46 +0800

    num83 num88.

--- num83.py
+++ num83.py
@@ -1,17 +1,26 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def deleteDuplicates(self, head: ListNode) -> ListNode:
-        look_up = []
 
+        if not head:
+            return head
 
+        pre, cur = head, head.next
 
+        while True:
 
+            while cur and cur.val == pre.val:
+                cur = cur.next
 
+            pre.next = cur
+            pre = cur
+            if not pre: break
 
+            cur = cur.next
 
+        return head
